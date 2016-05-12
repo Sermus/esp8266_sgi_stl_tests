ae26c9053e0fb44f254b9945399c71ec58fcd3d9
sermus <andrey.v.filimonov@gmail.com>
2016-05-12 13:11:37 +0300

    Tests are run in a separate RTOS task

--- user/user_main.cpp
+++ user/user_main.cpp
@@ -1,12 +1,22 @@
 #include <cpp_routines.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 #include "stl_test.h"
 extern "C"
 {
 #include <esp_common.h>
 }
 
+void test_task(void *pvParameters)
+{
+    os_printf("Test task\n");    
+    stl_main(1, NULL);
+    vTaskDelete(NULL);
+}
+
 extern "C" void user_init(void)
 {
     do_global_ctors();
-    stl_main(1, NULL);
+    os_printf("Creating test task\n");
+    xTaskCreate(test_task, "test_task", 1024, NULL, 1, NULL);
 }
